1a15477bc394b75b8162bd854e473e64ab25dd87
peterostrander2 <63732623+peterostrander2@users.noreply.github.com>
2026-01-07 11:40:52 -0500

    Update Signals.jsx

--- Signals.jsx
+++ Signals.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../services/api';
+import api from './api';
 
 const Signals = () => {
   const [activeTab, setActiveTab] = useState('live');
