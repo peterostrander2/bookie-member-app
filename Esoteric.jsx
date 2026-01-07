4c66af2fa3f51c95f9e53ee698bd2eb642b9a930
peterostrander2 <63732623+peterostrander2@users.noreply.github.com>
2026-01-07 11:40:28 -0500

    Update Esoteric.jsx

--- Esoteric.jsx
+++ Esoteric.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../services/api';
+import api from './api'
 
 const Esoteric = () => {
   const [todayEnergy, setTodayEnergy] = useState(null);
